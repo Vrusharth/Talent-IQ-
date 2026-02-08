e79bc6f5371e849054dee338b4a861d62daa84c0
Vrusharth <vrusharth2003@gmail.com>
2026-02-08 18:34:34 +0530

    Deployment Setup

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,11 +1,25 @@
 import express from 'express'
 import ENV from './lib/env.js';
-
+import path from 'path'
 const app = express();
 
-app.get('/',(req,res)=>{
+const __dirname=path.resolve()
+
+app.get('/health',(req,res)=>{
     res.status(200).json({msg:"Success from the api"})
 })
+
+app.get('/books',(req,res)=>{
+    res.status(200).json({msg:"Success from the api Books"})
+})
+
+if(ENV.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../Client/dist")))
+
+    app.get("/{any}", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../Client","dist","index.html"))
+    })
+}
 
 app.listen(ENV.PORT, ()=>{
     console.log("Server is runnning on a port",ENV.PORT)
